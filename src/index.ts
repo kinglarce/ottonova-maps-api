0eb0a0447a92162c97267c995d9c23956ffa3cc8
King Larce Cortes <kingcort01@gmail.com>
2022-01-14 17:30:25 +0800

    build: Set CORS header policy

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,12 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
+app.use((_, _res, next) => {
+  _res.header("Access-Control-Allow-Origin", "*");
+  _res.header("Access-Control-Allow-Headers", "Content-Type");
+  _res.header("Access-Control-Expose-Headers", "*");
+  next();
+});
 app.use(Router);
 
 app.listen(PORT, () => {
